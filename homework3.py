7d173012fea78451ffdef4990b3b7557f35defe4
GrafBayan <sashasashavasua@mail.ru>
2024-06-19 15:54:29 +0300

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,8 @@
+name = 'ALexandr'
+print('Name: ' + name)
+age = 21
+print('Age: ' + str(age))
+age = age + -5
+print('New age: ' + str(age))
+is_student = True
+print('Is student: ' + str(is_student))
