5e79055f1d56419e825ebbef4c45d39fd378d26e
GrafBayan <sashasashavasua@mail.ru>
2024-06-25 00:35:53 +0300

    Add files via upload

---
+++ Modul_2_2.py
@@ -0,0 +1,9 @@
+First = input('Enter the  first number:')
+Second = input('Enter the second number:')
+Third = input('Enter the  third number:')
+if First == Second and First == Third:
+    print(3)
+elif First == Second or First == Third:
+    print(2)
+else:
+    print(0)
