7a1677e19537a151ddd908cbf4695406dc613576
GrafBayan <sashasashavasua@mail.ru>
2024-06-27 00:37:25 +0300

    Update Modul_2_2.py

--- Modul_2_2.py
+++ Modul_2_2.py
@@ -1,9 +1,9 @@
-First = input('Enter the  first number:')
+First = input('Enter the first number:')
 Second = input('Enter the second number:')
-Third = input('Enter the  third number:')
+Third = input('Enter the third number:')
 if First == Second and First == Third:
-    print(3)
-elif First == Second or First == Third:
-    print(2)
+print(3)
+elif First == Second or First == Third or Second == Third:
+print(2)
 else:
-    print(0)
+print(0)
