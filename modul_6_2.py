390d47b682e8a62adf58baf6f196411803cec0e0
GrafBayan <sashasashavasua@mail.ru>
2024-08-27 22:07:37 +0300

    Update modul_6_2.py

--- modul_6_2.py
+++ modul_6_2.py
@@ -37,13 +37,10 @@ class Sedan(Vehicle):
 
 vehicle1 = Sedan('Fedos', 'Toyota Mark II', 500, "blue")
 
-# Изначальные свойства
 vehicle1.print_info()
 
-# Меняем свойства (в т.ч. вызывая методы)
 vehicle1.set_color('Pink')
 vehicle1.set_color('BLACK')
 vehicle1.owner = 'Vasyok'
 
-# Проверяем что поменялось
 vehicle1.print_info()
